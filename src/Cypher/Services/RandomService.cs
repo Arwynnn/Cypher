a9251924ceffa594fc1902926158c7201e2eb141
Dustin Lopez <dustin_lopez@hotmail.com>
2019-11-16 14:52:04 +0000

    Create RandomService.cs

---
+++ src/Cypher/Services/RandomService.cs
@@ -0,0 +1,39 @@
+﻿using Discord.WebSocket;
+
+namespace Cypher.Services
+{
+    public class RandomService
+    {
+        //Field
+        private readonly DiscordSocketClient _client;
+        
+        //Constructor (ctor)
+        public RandomService(DiscordSocketClient client)
+        {
+            _client = client;
+        }
+
+        public string CheckUserIsBot(SocketGuildUser user = null)
+        {
+            if (user is null)
+            {
+                return "You need to enter a user.";
+            }
+
+            if (user.Id == _client.CurrentUser.Id)
+            {
+                return "Of course I am a bot, you imbecile! 👿";
+             
+            }
+
+            if (user.IsBot)
+            {
+                return $"{user.Username} is a good bot :)";
+            }
+            else
+            {
+                return $"{user.Username} is not a good bot >:(";
+            }
+        }
+    }
+}
